012e9624d0993bb930adcf53dbeb5dd370b66684
Brayan Rincón <brayanrinconmendoza@gmail.com>
2020-10-21 08:01:20 -0500

    Quienes somos
    Sección Quienes Somos en Home

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,18 @@
         </v-row>
       </v-container>
     </div>
+    <div class="content-home">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="8" md="6">
+            <div>
+              <h1>Quienes somos</h1>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos aspernatur nulla non laudantium labore, ipsa repudiandae, neque et, fuga possimus aliquid officia esse consequatur? Aspernatur modi laudantium quisquam fuga eveniet?</p>
+            </div> 
+          </v-col>  
+        </v-row>
+      </v-container>
+    </div>
   </section>
 </template>
 
@@ -28,5 +40,9 @@
 <style lang="scss">
   .main-section {
     background-color: white; 
+  }
+
+  .content-home {
+    color: #000000;
   }
 </style>
