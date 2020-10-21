5438fc80a1c639ef7920c1cc634ced5d1ee82fb8
BrayanRinconMendoza <brayanrinconmendoza@gmail.com>
2020-10-20 22:58:52 -0500

    Estructura inicial

---
+++ pages/inspire.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <v-btn>Botón</v-btn> 
+      <div>
+        <Carousel /> 
+      </div>
+      <div>
+        <Form />  
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Carousel from '~/components/Carousel.vue'
+import Form from '~/components/Form.vue'
+
+export default {
+  components: {
+    Carousel,
+    Form 
+  }
+}
+</script>
